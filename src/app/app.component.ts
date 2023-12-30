b62df2c7118cb0b883eb0b833026d12c5f9c24ed
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-30 18:27:31 -0300

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./pages/login/login.component";
+import { MainComponent } from "./pages/main/main.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LoginComponent]
+    imports: [CommonModule, RouterOutlet, LoginComponent, MainComponent]
 })
 export class AppComponent {
   title = 'triagem-med';
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { CentralComponent } from "../../components/central/central.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+    selector: 'app-main',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css',
+    imports: [NavbarComponent, CentralComponent, FooterComponent]
 })
 export class MainComponent {
 
