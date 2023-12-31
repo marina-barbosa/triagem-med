6cdf3cbadf3395e63fa83da95c63487e1c805b63
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-31 06:10:34 -0300

    att sidebar service

--- src/app/services/sidebar-service.service.ts
+++ src/app/services/sidebar-service.service.ts
@@ -9,9 +9,23 @@ export class SidebarServiceService {
   private _sideOff = new BehaviorSubject<boolean>(false);
   public sideOff$ = this._sideOff.asObservable();
 
-  constructor() { }
+  constructor() {
+    window.addEventListener('resize', this.onWindowResize.bind(this));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.onWindowResize.bind(this));
+  }
+
+  onWindowResize(event: Event) {
+    const width = (event.target as Window).innerWidth;
+    if (width > 1200) {
+      this._sideOff.next(false);
+    }
+  }
 
   toggleSidebar(): void {
     this._sideOff.next(!this._sideOff.value);
   }
+
 }
