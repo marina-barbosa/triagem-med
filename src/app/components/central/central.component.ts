c4821cd4a716e50e314b2a47ecf00db17687d49f
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-31 20:35:15 -0300

    add form-cad-cidadao

--- src/app/components/central/central.component.ts
+++ src/app/components/central/central.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
+import { FormCadCidadaoComponent } from "../form-cad-cidadao/form-cad-cidadao.component";
 
 @Component({
     selector: 'app-central',
     standalone: true,
     templateUrl: './central.component.html',
     styleUrl: './central.component.css',
-    imports: [FooterComponent]
+    imports: [FooterComponent, FormCadCidadaoComponent]
 })
 export class CentralComponent {
 
